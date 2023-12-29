1c8b1d64884cdf31d7ab2ef7d04619cb4783ceaf
Taha cemal Aybakan <taybakan@k1m07s05.42istanbul.com.tr>
2023-12-29 12:35:22 +0300

    asf

---
+++ App.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <div className="row">
+        <div className="col">
+          <div className="image-container">
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 1"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 2"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="col">
+          <div className="image-container">
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 3"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 4"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="col">
+          <div className="image-container">
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 5"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 6"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer'}}
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="col">
+          <div className="image-container">
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 7"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+            <img
+              src="https://cdn.digitalage.com.tr/uploads/2014/05/Yandexin-g%C3%B6rsel-ile-g%C3%B6rsel-arama-%C3%B6zelli%C4%9Fi-hizmetinizde.jpg"
+              alt="Project 8"
+              style={{ objectFit: 'cover', width: '50%', height: '100%', cursor: 'pointer' }}
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
